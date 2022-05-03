064ec7e7d86fe3876c414355e186def225351a14
sagar-aryal <sagar_yal92@outlook.com>
2022-05-03 13:52:53 +0300

    Navbar component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 import Routes from './Routes'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Routes />
     </>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <span>Theme</span>
+        <h1>Countries</h1>
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <span>Cart</span>
+    </>
+  )
+}
+
+export default Navbar
